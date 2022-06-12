d2c6c095a829b77de934472338da3c8c15ac4312
fishworm96 <245525860@qq.com>
2022-06-12 23:11:31 +0800

    md5加盐

--- go.mod
+++ go.mod
@@ -4,9 +4,13 @@ go 1.18
 
 require gorm.io/gorm v1.23.6
 
-require github.com/go-sql-driver/mysql v1.6.0 // indirect
+require (
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	golang.org/x/crypto v0.0.0-20220525230936-793ad666bf5e // indirect
+)
 
 require (
+	github.com/anaskhan96/go-password-encoder v0.0.0-20201010210601-c765b799fd72
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.4 // indirect
 	gorm.io/driver/mysql v1.3.4
--- user_srv/model/main/main.go
+++ user_srv/model/main/main.go
@@ -1,39 +1,62 @@
 package main
 
 import (
-	"log"
-	"os"
-	"time"
+	"crypto/md5"
+	"crypto/sha512"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"strings"
 
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
-	"gorm.io/gorm/schema"
+	// "log"
+	// "os"
+	// "time"
 
-	"mxshop_srvs/user_srv/model"
+	// "gorm.io/driver/mysql"
+	// "gorm.io/gorm"
+	// "gorm.io/gorm/logger"
+	// "gorm.io/gorm/schema"
+	"github.com/anaskhan96/go-password-encoder"
+	// "mxshop_srvs/user_srv/model"
 )
 
+func genMd5(code string) string {
+	Md5 := md5.New()
+	_, _ = io.WriteString(Md5, code)
+	return hex.EncodeToString((Md5.Sum(nil)))
+}
+
 func main() {
-	dsn := "root:root@tcp(localhost:3306)/mxshop_user_srv?charset=utf8mb4&parseTime=True&loc=Local"
+	// dsn := "root:root@tcp(localhost:3306)/mxshop_user_srv?charset=utf8mb4&parseTime=True&loc=Local"
 
-	newLogger := logger.New(
-		log.New(os.Stdout, "\r\n", log.LstdFlags),
-		logger.Config{
-			SlowThreshold: time.Second,
-			LogLevel:      logger.Info,
-			Colorful:      true,
-		},
-	)
+	// newLogger := logger.New(
+	// 	log.New(os.Stdout, "\r\n", log.LstdFlags),
+	// 	logger.Config{
+	// 		SlowThreshold: time.Second,
+	// 		LogLevel:      logger.Info,
+	// 		Colorful:      true,
+	// 	},
+	// )
 
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
-		NamingStrategy: schema.NamingStrategy{
-			SingularTable: true,
-		},
-		Logger: newLogger,
-	})
-	if err != nil {
-		panic(err)
-	}
+	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+	// 	NamingStrategy: schema.NamingStrategy{
+	// 		SingularTable: true,
+	// 	},
+	// 	Logger: newLogger,
+	// })
+	// if err != nil {
+	// 	panic(err)
+	// }
 
-	_ = db.AutoMigrate(&model.User{})
+	// _ = db.AutoMigrate(&model.User{})
+
+	options := &password.Options{16, 100, 32, sha512.New}
+	salt, encodePwd := password.Encode("generic password", options)
+	newPassword := fmt.Sprintf("$pbkdf2-sha512$%s$%s", salt, encodePwd)
+	fmt.Println(newPassword)
+
+	passwordInfo := strings.Split(newPassword, "$")
+	fmt.Println(passwordInfo)
+	check := password.Verify("generic password", passwordInfo[2], passwordInfo[3], options)
+	fmt.Println(check)
 }
